5b194efa1cb81b9bba4e36c053e8bcb0d2725525
minnukota381 <minnukota381@gmail.com>
2024-12-13 22:50:37 +0530

    Stack Data Structure

--- src/StackDemo/StackExample.java
+++ src/StackDemo/StackExample.java
@@ -1,41 +1,19 @@
 package StackDemo;
 
-//import java.util.Stack;
-//
-//public class StackExample {
-//	public static void main(String[] args) {
-//		Stack<Integer> st = new Stack<>();
-//
-//		st.push(10);
-//		st.push(20);
-//		st.push(40);
-//
-//		System.out.println("Highest Element : " + st.peek());
-//		System.out.println("Popped element : " + st.pop());
-//		System.out.println("Empty or not :" + st.isEmpty());
-//		System.out.println(st);
-//
-//	}
-//}
-
-class X {
-
-	Dolly s; // Dolly is a data type.
-	int d = 10; // number
-
-	class Dolly {
-		int g = 9;
-		double x = 0.7;
-		String name = "Seven";
-	}
-
-	void display() {
-
-	}
-}
+import java.util.Stack;
 
 public class StackExample {
 	public static void main(String[] args) {
+		Stack<Integer> st = new Stack<>();
+
+		st.push(10);
+		st.push(20);
+		st.push(40);
+
+		System.out.println("Highest Element : " + st.peek());
+		System.out.println("Popped element : " + st.pop());
+		System.out.println("Empty or not :" + st.isEmpty());
+		System.out.println(st);
 
 	}
 }
