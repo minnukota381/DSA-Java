2214d84bcddc1a810aa2d582770094bd5266423a
minnukota381 <minnukota381@gmail.com>
2025-01-16 00:19:56 +0530

    Implement Insertion Sort algorithm with user input functionality

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,46 @@
+package Sorting;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        insertionSort(arr);
+
+        System.out.println("Sorted array: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        scanner.close();
+    }
+}
+
